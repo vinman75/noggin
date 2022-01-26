1b3ceb8e51d44eb7c1b5b9f9c234a26a4b61d3a5
Vinman <vincent.rossini@gmail.com>
2022-01-27 08:29:38 +1100

    Updats

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# main.py
+# noggin.py
+
 import PySimpleGUI as sg
 import os.path
 import re
@@ -9,46 +10,31 @@ ic = "nog.ico"
 
 
 def rename(original):
-    layout = [[sg.Text('File Name:')],
-              [sg.Input(original[:-4])],
-              [sg.Button('Okay', size=(39, 1), enable_events=True,
-                         key='-REN-', bind_return_key=True)]
-              ]
-    popWin = sg.Window('Noggin Name', layout, icon=ic)
-
-    while True:     # Event Loop
-        event, values = popWin.read()
-
-        if event == '-REN-' or event == sg.WIN_CLOSED:
-            print(values[0])
-            break
-    popWin.close()
+    layout = [[sg.T('File Name:')],
+              [sg.I(original[:-4])],
+              [sg.Ok(size=(39, 1), bind_return_key=True)]]
+    window = sg.Window('Noggin Name', layout, icon=ic)
+    event, values = window.read(close=True)
     return values[0]
 
 
-def confirm():
-    filename = clean_str()
-    layout = [[sg.Text('Are you sure?')],
-              [sg.Button('Yes', size=(20, 1), enable_events=True, key='-CONFIRM-'),
-               sg.Button('No', size=(20, 1), enable_events=True, key='-CANCEL-')]]
-
-    confirmWin = sg.Window('Delete: {}'.format(filename), layout, icon=ic)
-
-    while True:     # Event Loop
-        event, values = confirmWin.read()
-
-        if event == sg.WIN_CLOSED:
-            break
-
-        if event == '-CONFIRM-':
-            name = clean_str()
+def delete_noggin():
+    filename = str_name()
+    if filename:
+        layout = [[sg.T('Are you sure?')],
+                  [sg.Ok(size=(20, 1)),
+                   sg.Cancel(size=(20, 1))]]
+        window = sg.Window(f'Delete: {filename}', layout, icon=ic)
+        event, values = window.read()
+        if event == 'Ok':
+            name = str_name()
             del_noggin(name)
+            window.close()
         else:
-            pass
-        confirmWin.close()
+            window.close()
 
 
-def clean_str():
+def str_name():
     try:
         inputString = values['-NAME-']
         name = re.findall("\'(.*?)\'", inputString)
@@ -63,7 +49,7 @@ def refresh_entries():
 
 
 def load_entry(item):
-    with open('entries/{}'.format(item[0])) as f:
+    with open(f'entries/{item[0]}') as f:
         text = f.read()
     window['-MULTI-'].update(text)
 
@@ -71,27 +57,27 @@ def load_entry(item):
 def new_noggin():
     filename = rename('')
     if filename:
-        with open('entries/{}.nog'.format(filename), mode='a'):
+        with open(f'entries/{filename}.nog', mode='a'):
             window['-LIST-'].update(refresh_entries())
+            window['-NAME-'].update([f'{filename}.nog'])
+            load_entry([f'{filename}.nog'])
 
 
-def rename_old():
+def rename_old(old_name):
     try:
-        inputString = values['-NAME-']
-        old_name = re.findall("\'(.*?)\'", inputString)
-        filename = rename(clean_str())
+        filename = rename(str_name())
         if filename:
-            os.rename('entries/{}'.format(old_name[0]),
-                      'entries/{}.nog'.format(filename))
-            window['-NAME-'].update('{}.nog'.format(filename))
+            os.rename(f'entries/{old_name}', f'entries/{filename}.nog')
             window['-LIST-'].update(refresh_entries())
+            window['-NAME-'].update([f'{filename}.nog'])
+            load_entry([f'{filename}.nog'])
     except TypeError:
         pass
 
 
 def del_noggin(item):
     try:
-        os.remove('entries/{}'.format(item))
+        os.remove(f'entries/{item}')
         window['-MULTI-'].update("")
         window['-NAME-'].update("")
         window['-LIST-'].update(refresh_entries())
@@ -102,7 +88,7 @@ def del_noggin(item):
 def update_noggin(item):
     if values['-NAME-']:
         try:
-            with open('entries/{}'.format(item), mode='w') as f:
+            with open(f'entries/{item}', mode='w') as f:
                 content = values['-MULTI-']
                 f.write(content)
                 window['-LIST-'].update(refresh_entries())
@@ -110,36 +96,52 @@ def update_noggin(item):
             pass
 
 
+def global_search(gword):
+    path = ('entries')
+    entries = []
+    files = os.listdir(path)
+    for file in files:
+        with open(os.path.join(path, file), 'r') as f:
+            if re.search(gword, f.read(), re.IGNORECASE):
+                entries.append(file)
+    return(entries)
+
+
 # window layout of the columns
-entries_column = [[sg.Button('New', size=(6, 1), enable_events=True, key='-NEW-'), sg.Text('Noggin Entries:')],
+# window layout of the columns
+frame = [[sg.Radio('Title Search', group_id=1,  default=True, key='-LOCAL-'), sg.Radio('Keyword Search', group_id=1, key='-GLOBAL-')],
+         [sg.Text('Filter:'), sg.Input(size=(28, 1), enable_events=True, key='-FILTER-')]]
+
+entries_column = [[sg.Button('New', size=(6, 1), key='-NEW-'), sg.Text('Noggin Entries:')],
                   [sg.Listbox(refresh_entries(), size=(33, 23),
                               enable_events=True, key="-LIST-")],
-                  [sg.Text('Filter:'), sg.Input(size=(30, 1), enable_events=True, key='-INPUT-')]]
+                  [sg.Frame('Search Method', frame)]]
 
-read_column = [[sg.Button('Save', size=(16, 1), enable_events=True, key='-SAVE-'),
-                sg.Button('Rename', size=(17, 1),
-                          enable_events=True, key='-REN-'),
-                sg.Button('Delete', size=(17, 1), enable_events=True, key='-DEL-')],
-               [sg.Text('Viewing:'), sg.Input(size=(53, 1), readonly=False,
-                                              enable_events=True, key='-NAME-')],
-               [sg.Multiline(size=(60, 24),  key="-MULTI-")]]
+read_column = [[sg.Button('Save', size=(16, 1), key='-SAVE-'),
+                sg.Button('Rename', size=(17, 1), key='-REN-'),
+                sg.Button('Delete', size=(17, 1), key='-DEL-')],
+               [sg.Text('Viewing:'), sg.Input(size=(53, 1), key='-NAME-')],
+               [sg.Multiline(size=(60, 27),  key="-MULTI-")]]
 
 layout = [[sg.Column(entries_column),
            sg.VSeperator(),
            sg.Column(read_column)]]
 
 window = sg.Window('Noggin v1.0 - V.Rossini', layout, icon=ic)
-#window = make_window()
 
 # Event loop
 while True:
     event, values = window.read()
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
-    if values['-INPUT-'] != '':
-        search = values['-INPUT-']
+    if values['-FILTER-'] != '' and values['-LOCAL-']:
+        search = values['-FILTER-']
         filtered = [x for x in refresh_entries() if search in x]
         window['-LIST-'].update(filtered)
+    elif values['-FILTER-'] != '' and values['-GLOBAL-']:
+        search = values['-FILTER-']
+        filter = global_search(search)
+        window['-LIST-'].update(filter)
     else:
         window['-LIST-'].update(refresh_entries())
     if event == '-LIST-' and len(values['-LIST-']):
@@ -148,10 +150,10 @@ while True:
     if event == '-NEW-':
         new_noggin()
     if event == '-DEL-':
-        confirm()
+        delete_noggin()
     if event == '-SAVE-':
-        name = clean_str()
-        update_noggin(name)
+        update_noggin(str_name())
     if event == '-REN-':
-        rename_old()
+        rename_old(str_name())
+
 window.close()
